24e0b8ee9d5f0afd49fb7b56c4830d7fcc7ec232
Matt Pagel <MPagel@users.noreply.github.com>
2015-07-12 09:09:22 -0700

    moved author

--- multi_seqdoc.pl
+++ multi_seqdoc.pl
@@ -1,11 +1,4 @@
 #!/usr/bin/perl
-=head1 AUTHOR
-The single file version of this script was published as Crowe, Mark L "SeqDoC: rapid SNP and mutation detection by direct comparison of DNA sequence chromatograms." BMC Bioinformatics 2005, 6:133  doi:10.1186/1471-2105-6-133
-with an attached Creative Commons license: (http://creativecommons.org/licenses/by/2.0).
-"Multiple SeqDoC" by Mark Crowe was obtained from website at http://research.imb.uq.edu.au/seqdoc/, specifically http://research.imb.uq.edu.au/seqdoc/multi_seqdoc.pl
-
-Other modifications performed by Matt Pagel to read secondary peak-under-peak
-
 =head1 NAME
 
 multi_seqdoc.pl - Perl cgi script to align and subtractively compare multiple sequence
@@ -67,6 +60,12 @@ http://research.imb.uq.edu.au/seqdoc/
 Written by Mark Crowe - mcrowe@bioinformatics.org
 
 The Institute for Molecular Bioscience, The University of Queensland, Brisbane, Queensland, Australia.
+
+The single file version of this script was published as Crowe, Mark L "SeqDoC: rapid SNP and mutation detection by direct comparison of DNA sequence chromatograms." BMC Bioinformatics 2005, 6:133  doi:10.1186/1471-2105-6-133
+with an attached Creative Commons license: (http://creativecommons.org/licenses/by/2.0).
+"Multiple SeqDoC" by Mark Crowe was obtained from website at http://research.imb.uq.edu.au/seqdoc/, specifically http://research.imb.uq.edu.au/seqdoc/multi_seqdoc.pl
+
+Other modifications performed by Matt Pagel to read secondary peak-under-peak
 
 =head1 COPYRIGHT
 
